ec9499ac72a619b8a07b20c6a42e33f4893a8cac
pino <tkinoue0324@gmail.com>
2024-02-10 14:04:43 +0900

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,29 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 // この関数はすべてのページリクエストに対して実行されます
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // 認証が不要なページのパスを指定
-  if (pathname.includes('/login')) {
+  // pathname確認
+  console.log(pathname);
+
+  // 認証が不要なページやアセット、APIリクエストのパスを指定
+  if (
+    pathname.includes("/login") ||
+    pathname.startsWith("/_next/")
+    // || pathname.startsWith("/api/") // APIリクエストをスルーさせるかは微妙
+  ) {
     return NextResponse.next();
   }
 
   // ここで認証状態をチェックする（例：セッションCookieを確認）
   // 仮に認証状態を示すセッションCookieが存在しないとします
-  const userAuthenticated = request.cookies.get('login_info');
+  const userAuthenticated = request.cookies.get("login_info");
 
   // ユーザーが未認証の場合、ログインページにリダイレクト
   if (!userAuthenticated) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
